a20694dfcc6551115560fa9f5ae79fed30da3325
Cristiliano Júnior <cristilianojunior@gmail.com>
2021-11-16 12:34:01 -0300

    add setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Pyon (Python Object Notation / .pyon)'
+LONG_DESCRIPTION = 'Pyon (Python Object Notation) is a file that stores data in python code style and are .pyon files'
+
+# Setting up
+setup(
+    name="pyon",
+    version=VERSION,
+    author="Cristiliano Junior",
+    author_email="<cristilianojunior@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'pyon'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
