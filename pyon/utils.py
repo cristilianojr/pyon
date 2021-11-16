d0e442301764d690528facf0c99ec147e4c000aa
Cristiliano Júnior <cristilianojunior@gmail.com>
2021-11-16 13:07:52 -0300

    Update utils.py

--- pyon/utils.py
+++ pyon/utils.py
@@ -7,6 +7,7 @@ def indent_string(string: str, apply_indentation_with: int = 4) -> str:
     indent_counter = 0
     indent_space = ' ' * apply_indentation_with
     char_list = [c for c in string]
+    before_char: str = ''
 
     for char in enumerate(char_list):
         if char[1] == '{' or char[1] == '[':
@@ -20,5 +21,9 @@ def indent_string(string: str, apply_indentation_with: int = 4) -> str:
         elif char[1] == ',':
             c = char_list.pop(char[0])
             char_list.insert(char[0], c + '\n' + indent_space * indent_counter)
-
+        elif char[1] == ' ' and before_char == ',':
+            c = char_list.pop(char[0])
+            char_list.insert(char[0], '')
+        before_char = char[1]
+        
     return ''.join(char_list)
